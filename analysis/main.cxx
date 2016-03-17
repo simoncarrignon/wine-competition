7d7c79ce9d9650c3f22c58045c2ef2db579d86f5
Jean-Marc <montanier.jeanmarc@gmail.com>
2016-03-17 12:11:14 +0100

    analyse evo results

--- analysis/main.cxx
+++ analysis/main.cxx
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 // 	const std::vector<std::string> TYPES = {"all", "mdp", "random"};
 // 	const std::vector<std::string> TYPES = {"mdp", "random"};
 //	const std::vector<std::string> TYPES = {"mdp", "random", "lazy", "greedy"};
-	const std::vector<std::string> TYPES = {"mdp", "random", "lazy", "greedy", "learning", "sarsa"};
+	const std::vector<std::string> TYPES = {"mdp", "random", "lazy", "greedy", "learning", "sarsa", "evo"};
 //	const std::vector<std::string> TYPES = {"learning"};
 	for (const auto& type:TYPES) {
 		std::string output(std::string(argv[2]) + "/agent-" + type + ".csv");
